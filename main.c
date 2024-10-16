ce95e48463178cad4b2d7fc3515793d279a0e6bd
Johan Andersson <johan.indrit.andersson@protonmail.com>
2024-10-16 20:26:59 +0200

    cmd

--- main.c
+++ main.c
@@ -1,15 +1,60 @@
 #include <wchar.h>
 #include <windows.h>
 #include <wlanapi.h>
-
-#define TARGET_SSID "TheCrib 5GHz" /// Name
-#define TARGET_SECURITY TRUE /// HAS WAEP2 Security
-#define TARGET_BSSTYPE 1 /// Infrastructre
-#define TARGET_EXPECTED_RESPONSE 1 /// Only one AP should pop up
+#include <getopt.h>
+#include <stdio.h>
 
 #define MAX_CLIENT 2 /// https://learn.microsoft.com/en-us/windows/win32/api/wlanapi/nf-wlanapi-wlanopenhandle
 
-int main(void) {
+static struct option long_options[] =
+        {
+                {"ssid", required_argument, NULL, 's'},
+                {"password", required_argument, NULL, 'p'},
+                {"bss_type", required_argument, NULL, 'b'},
+                {"expected", required_argument, NULL, 'e'}
+        };
+
+int main(int argc, char *argv[]) {
+    int ch;
+    char *ssid = NULL;
+    BOOL password = FALSE;
+    int bssType = 1;
+    int expected = 1;
+
+    if (argc != 9) {
+        wprintf(L"Incorrect amount of arguments passed to function (%d)\n", argc);
+        wprintf(L"Expected argument is: \n", argc);
+        wprintf(L"\t-s (ssid): (string)\n");
+        wprintf(L"\t-p (password): (y/n)\n");
+        wprintf(L"\t-b (bss_type): (1/2/3)\n");
+        wprintf(L"\t-e (expected): (int)\n");
+        return 1;
+    }
+
+    while ((ch = getopt_long(argc, argv, "s:p:b:e:", long_options, NULL)) != -1)
+    {
+        switch (ch)
+        {
+            case 's':
+                ssid = optarg;
+                break;
+            case 'p':
+                if(strcmp(optarg, "Y") == 0 || strcmp(optarg, "y") == 0)
+                    password = TRUE;
+                if(strcmp(optarg, "N") == 0 || strcmp(optarg, "n") == 0)
+                    password = FALSE;
+                else
+                    wprintf(L"Only allowed values are Y/y (yes) or N/n (no)");
+                break;
+            case 'b':
+                bssType = strtol(optarg, NULL, 10); // or copy it if you want to
+                break;
+            case 'e':
+                expected = strtol(optarg, NULL, 10); // or copy it if you want to
+                break;
+        }
+    }
+
     HANDLE hClient = NULL;
     DWORD dwCurVersion = 0;
     DWORD dwResult = 0;
@@ -44,7 +89,6 @@ int main(void) {
         for (i = 0; i < pIfList->dwNumberOfItems; i++) {
             pIfInfo = (WLAN_INTERFACE_INFO *) &pIfList->InterfaceInfo[i];
 
-//            WlanRegisterNotification(hClient, WLAN_NOTIFICATION_SOURCE_ACM, TRUE, )
             // Flush buffers and populate with latest wifi scan data
             dwResult = WlanScan(hClient, &pIfInfo->InterfaceGuid, NULL, NULL, NULL);
 
@@ -58,33 +102,17 @@ int main(void) {
             wprintf(L"Interface Index: %d\n", i);
             wprintf(L"  InterfaceDescription: %ls\n", pIfInfo->strInterfaceDescription);
 
+            DOT11_SSID target;
+            strcpy(target.ucSSID, ssid);
+            target.uSSIDLength = (ULONG) strlen(ssid);
 
-            /**
-             * pDot11Ssid IS NULL = ALL BSS ARE LISTED https://learn.microsoft.com/en-us/windows/win32/api/wlanapi/nf-wlanapi-wlangetnetworkbsslist
-             * dot11BssType IGNORED https://learn.microsoft.com/en-us/windows/win32/nativewifi/dot11-bss-type
-             * bSecurityEnabled IGNORED
-             **/
-            if(TARGET_SSID == NULL) {
-                dwResult = WlanGetNetworkBssList(hClient,
-                                                 &pIfInfo->InterfaceGuid,
-                                                 NULL,
-                                                 3,
-                                                 TRUE,
-                                                 NULL,
-                                                 &pBssList);
-            } else {
-                DOT11_SSID target;
-                strcpy(target.ucSSID, TARGET_SSID);
-                target.uSSIDLength = (ULONG) strlen(TARGET_SSID);
-
-                dwResult = WlanGetNetworkBssList(hClient,
-                                                 &pIfInfo->InterfaceGuid,
-                                                 &target,
-                                                 TARGET_BSSTYPE,
-                                                 TARGET_SECURITY,
-                                                 NULL,
-                                                 &pBssList);
-            }
+            dwResult = WlanGetNetworkBssList(hClient,
+                                             &pIfInfo->InterfaceGuid,
+                                             &target,
+                                             bssType,
+                                             password,
+                                             NULL,
+                                             &pBssList);
 
             if (dwResult != ERROR_SUCCESS) {
                 wprintf(L"WlanGetNetworkBssList failed with error: %u\n",
@@ -116,12 +144,12 @@ int main(void) {
             }
 
             wprintf(L"\n");
-            if(TARGET_EXPECTED_RESPONSE > 0 && TARGET_EXPECTED_RESPONSE < pBssList->dwNumberOfItems) {
+            if(expected < pBssList->dwNumberOfItems) {
                 wprintf(L"MitM THREAT SUSPECTED: \n");
-                wprintf(L"  EXPECTED: %d\n", TARGET_EXPECTED_RESPONSE);
+                wprintf(L"  EXPECTED: %d\n", expected);
                 wprintf(L"  FOUND: %d\n", pBssList->dwNumberOfItems);
             } else {
-                wprintf(L"NO MitM FOUND for %s", TARGET_SSID);
+                wprintf(L"NO MitM FOUND for %s", ssid);
             }
         }
     }
